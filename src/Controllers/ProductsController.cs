0809f6eb4584c7952ff36b51154fb43775595fd9
Koval, Matus <matus.kvl@gmail.com>
2021-09-13 14:39:40 +0200

    get all and get by id base implemented

---
+++ src/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using API.DataAccess;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailerInterviewAPITask.Controllers {
+    [ApiController]
+    [Route( "api/[controller]" )]
+    public class ProductsController : ControllerBase {
+        
+
+        private readonly ILogger<ProductsController> _logger;
+        private readonly ProductsDbContext _productsDbContext;
+
+        public ProductsController( ILogger<ProductsController> logger,ProductsDbContext productsDbContext ) {
+            _logger = logger;
+            _productsDbContext = productsDbContext;
+        }
+
+        [Produces( "application/json" )]
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetAll() {
+            return await _productsDbContext.Products.AsNoTracking().ToListAsync();
+        }
+
+        [Produces( "application/json" )]
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetById(int id) {
+            var product =  await _productsDbContext.Products.FindAsync( id );
+
+            if ( product == null )
+                return NotFound();
+
+            return Ok( product );
+        }
+
+        [Consumes("text/plain")]
+        [Produces( "application/json" )]        
+        [HttpPatch( "{id:int}/update-description" )]
+        public async Task<ActionResult<Product>> UpdateDescription( int id ,  string newDescription ) {
+
+            if ( newDescription ==null ) {
+                return BadRequest( "Missing DescriptionDto" );
+            }
+
+            var product = await _productsDbContext.Products.FindAsync( id );
+
+            if ( product == null )
+                return NotFound();
+
+            product.Description = newDescription;
+
+            await _productsDbContext.SaveChangesAsync();
+
+            return Ok( product );
+        }
+    }
+}
---
+++ src/Models/DescriptionDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models {
+    public class DescriptionDto {
+        [Required]
+        public string Description { get; set; }
+    }
+}
