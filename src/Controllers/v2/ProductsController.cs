cf2925eb1d3341c9f0aaf1a5a2dc3524067dd05d
Koval, Matus <matus.kvl@gmail.com>
2021-09-15 06:38:26 +0200

    pageination query from query implemented

--- src/Controllers/v2/ProductsController.cs
+++ src/Controllers/v2/ProductsController.cs
@@ -1,11 +1,13 @@
 ﻿using API.DataAccess;
 using API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,10 +17,26 @@ namespace RetailerInterviewAPITask.Controllers {
 
         [MapToApiVersion( "2.0" )]
         [Produces( "application/json" )]
+        [ProducesResponseType( StatusCodes.Status200OK, Type = typeof( Product ) )]
+        [ProducesResponseType( StatusCodes.Status404NotFound )]
         [HttpGet]
-        public async Task<PaginatedResponseModel<Product>> GetAll20( CancellationToken cancellationToken ) {
+        public async Task<ActionResult<PaginatedResponseModel<Product>>> GetAll20( 
+            [FromQuery] PaginationQuery paginationQuery, CancellationToken cancellationToken ) 
+        {
 
-            return await _productsDbContext.Products.AsNoTracking().PaginateAsync<Product>( 1, 10, cancellationToken );
+            try {
+                return Ok( await _productsDbContext
+                    .Products
+                    .AsNoTracking()
+                    .PaginateAsync<Product>( paginationQuery.Page, paginationQuery.PageSize, cancellationToken ) );
+
+            }
+            catch ( PageOutOfRangeException exception ) {
+              
+                return NotFound( (new { Error=  exception.GetType().Name , Message= exception.Message }) );
+            }
+
+
         }
        
     }
---
+++ src/Models/PaginationQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models {
+    public class PaginationQuery {
+        private int pageSize =10;
+
+        public int PageSize {
+            get => pageSize;
+            set  {
+                pageSize = value >100 ? 100 : value;
+            } }
+        public int Page { get; set; } = 1;
+
+    }
+}
